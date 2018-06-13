98a9f49d15c1c65fe70ce04cb6fec192cd2265c6
Popov Eugene <zhenya1995q@gmail.com>
2018-06-13 20:20:36 +0300

    gitignore+dustructor

---
+++ Person.php
@@ -0,0 +1,46 @@
+<?php
+class Person
+{
+    public static $counter = 0;
+    private $firstName = 'unnamed';
+    private $lastName;
+    public function __construct($firstName, $lastName)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        self::$counter++;
+    }
+    public function __destruct()
+    {
+        echo 'Person objects was created: ' . self::$counter . PHP_EOL;
+    }
+    // pubsf + tab
+    // prosf + tab
+    // prisf + tab
+    // pubf + tab
+    // prof + tab
+    // prif + tab
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+    public function setFirstName($firsName)
+    {
+        $this->firstName = $firsName;
+    }
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+}
+$person = new Person('Vasya', 'Petrov');
+var_dump($person->getFirstName(), $person->getLastName());
+$person->setFirstName('Vasya');
+$person->setLastName('Petrov');
+var_dump($person->getFirstName());
+var_dump($person->getLastName());
+$person2 = new Person('Nastya', 'Rezova');
